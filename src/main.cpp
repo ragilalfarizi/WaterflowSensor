de1dbc7e3383adec0739f09965f1829026278993
Ragil Alfarizi <alfariziragill@gmail.com>
2023-06-01 17:24:29 +0700

    Setting up MQTT environment

--- src/main.cpp
+++ src/main.cpp
@@ -29,6 +29,35 @@ unsigned int totalMilliLitres;
 float flowLitres;
 float totalLitres;
 
+void reconnect()
+{
+  // Loop until we're reconnected
+  while (!client.connected())
+  {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str()))
+    {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      // client.publish("outTopic", "hello world");
+      // // ... and resubscribe
+      // client.subscribe("inTopic");
+    }
+    else
+    {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
 void IRAM_ATTR pulseCounter()
 {
   pulseCount++;
@@ -74,10 +103,21 @@ void setup()
   previousMillis = 0;
 
   attachInterrupt(digitalPinToInterrupt(SENSOR), pulseCounter, FALLING);
+
+  client.setServer(mqtt_server, 1833);
+  // client.setCallback(callback);
 }
 
 void loop()
 {
+  if (!client.connected())
+  {
+    reconnect();
+  }
+  client.loop();
+  StaticJsonDocument<32> doc;
+  char output[55];
+
   currentMillis = millis();
   if (currentMillis - previousMillis > interval)
   {
